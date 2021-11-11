7076e5ac3d44868053eed5859ea3343ff7c6f166
3l_ChAp0 <83827672+AmoghBalepur03@users.noreply.github.com>
2021-11-11 22:21:00 +0530

    yoss

--- CNN_2.py
+++ CNN_2.py
@@ -45,6 +45,47 @@ def model_emnist():
     accuracy,loss=model.evaluate(x_test,y_test)
     print(accuracy)
     print(loss)
+
+def model():
+    mnist=tf.keras.datasets.mnist 
+    (x_train,y_train),(x_test,y_test)=mnist.load_data()
+
+
+    x_train = tf.keras.utils.normalize(x_train,axis=1)
+    x_test = tf.keras.utils.normalize(x_test,axis=1)
+
+
+    model= tf.keras.models.Sequential() 
+    model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+    model.add(tf.keras.layers.Dense(units=128,activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(units=128,activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(units=10,activation=tf.nn.softmax))
+    model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+    model.fit(x_train,y_train,epochs=10) 
+
+    loss,accuracy=model.evaluate(x_test,y_test)
+    print(accuracy)
+    print(loss)
+    model.save('digits2.model')
+
+def model_digits2_model():
+     model=load_model("digits2.model")# to avoid repeated training
+
+     image_resize_model("3.png")
+     img= cv.imread("3.png")[:,:,0]
+     img=np.invert(np.array([img]))
+     prediction=model.predict(img)
+     print('The result is probably:',np.argmax(prediction))
+     plt.imshow(img[0],cmap=plt.cm.binary)
+     plt.show() 
+                                     
+def MNIST_h5_model():
+    model=load_model("MNIST.h5")
+    img=image_resize_model("3.png")
+    res = model.predict([img])[0]
+    pred =  np.argmax(res)
+    print(pred)
+
  
 
 model=load_model(r"model/handwriting.model")# to avoid repeated training
